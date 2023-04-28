5fc16972d535d572104978f3453cdf0993ffc40e
Andreas Olsson <noname.olsson@gmail.com>
2023-04-28 15:49:10 +0200

    spelling

--- apps/blog/src/app/app.tsx
+++ apps/blog/src/app/app.tsx
@@ -24,7 +24,7 @@ export function App() {
   return (
     <div className={styles['app']}>
       <div className={styles['wrapper']}>
-        <Header title="Blog" description="This is an aswemos asdblog app" />
+        <Header title="Blog" description="This is an aswemoss asdblog app" />
       </div>
       <h4>{Uppercase('posts')}</h4>
       {error ? (
